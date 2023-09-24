283772f55b9241a02442b408b381faf2b6d55ffe
Anshul Caplish <anshulsar@gmail.com>
2023-09-24 21:25:43 +0530

    Using Functions to check Num Prime or not

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -9,7 +9,11 @@ public class PrimeNumber {
 		System.out.println("Enter number to check Prime or not");
 		int n = sc.nextInt();
 		sc.close();
-		boolean check = true;
+		findPrimeNum(n);
+		
+	}
+		public static void findPrimeNum (int n) {
+			boolean check = true;
 		if (n > 0) {
 			if (n > 1) {
 				for (int i = 2; i <= n / 2; i++) {
