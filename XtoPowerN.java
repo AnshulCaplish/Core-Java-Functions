d28337504b0973af53ba7fb7ec4b58da3c0e102d
Anshul Caplish <anshulsar@gmail.com>
2023-09-29 21:37:11 +0530

    Adding comments in Power problem

--- XtoPowerN.java
+++ XtoPowerN.java
@@ -2,6 +2,11 @@ package Functions;
 
 import java.util.Scanner;
 
+/**
+ * Two numbers are entered by the user, x and n. Write a function to find the value of one number raised 
+ * to the power of another i.e. xn.
+ *
+ */
 public class XtoPowerN {
 
 	public static void main(String[] args) {
