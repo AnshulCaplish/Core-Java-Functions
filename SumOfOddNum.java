865977982c77fae1979e2883892b1b3f3a829650
Anshul Caplish <anshulsar@gmail.com>
2023-09-30 19:20:32 +0530

    Function to get Sum of Odd numbers

--- SumOfOddNum.java
+++ SumOfOddNum.java
@@ -2,6 +2,10 @@ package Functions;
 
 import java.util.Scanner;
 
+/**
+ * Write a function to print the sum of all odd numbers from 1 to n.
+ *
+ */
 public class SumOfOddNum {
 
 	public static void main(String[] args) {
